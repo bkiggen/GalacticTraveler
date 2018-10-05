fea72a6ab85d87b923b3e9851459ce1b77d94750
Ben Kiggen <benkiggen@gmail.com>
2018-10-05 16:52:42 -0700

    add front end input and front end logic

--- src/main.js
+++ src/main.js
@@ -5,10 +5,17 @@ import GalacticTraveler from './backEnd.js';
 import $ from 'jquery';
 
 $(document).ready(function(){
-  $(".infoForm").submit(function(){
-
+  $(".infoForm").submit(function(event){
+    event.preventDefault();
+    let userDay = $(".day").val();
+    let userMonth = $(".month").val();
+    let userYear = $(".year").val();
+    let userGender = $(".gender").val();
+    let userLifestyle = $(".lifestyle").val();
+    let trav = new GalacticTraveler(userDay, userMonth, userYear, userGender, userLifestyle);
+    let trav2 = trav.calcAge().calcExpectancy().calcYearsLeft();
+    console.log(trav2);
   });
-  let trav = new GalacticTraveler("15", "1", "1984", "male", "healthy");
-  let trav2 = trav.calcAge().calcExpectancy().calcYearsLeft();
-  console.log(trav2);
+
+
 });
